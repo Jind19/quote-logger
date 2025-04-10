d18fcf15f1ae3a4fc7f66920dd8f9aea154ec574
Prachi <164371788+Jind19@users.noreply.github.com>
2025-04-10 14:46:12 +0200

    Update Quotes.java

--- Quotes.java
+++ Quotes.java
@@ -2,6 +2,10 @@ public class Quotes {
     public static void main(String[] args) {
 
         System.out.println("The only limit to our realization of tomorrow is our doubts of today.");
+
+        //Quote from Github
+        System.out.println("Believe in yourself!");
+
     }
 }
 
