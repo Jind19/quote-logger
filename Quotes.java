d3b614ccf3d4730e44211cdbd98a8be4cec2cd2e
Prachi Jindal <prachibansal135@gmail.com>
2025-04-10 14:47:19 +0200

    Add third quote locally

--- Quotes.java
+++ Quotes.java
@@ -1,7 +1,8 @@
 public class Quotes {
     public static void main(String[] args) {
-
+        //Quote from Intellij
         System.out.println("The only limit to our realization of tomorrow is our doubts of today.");
     }
 }
 
+// My third quote
